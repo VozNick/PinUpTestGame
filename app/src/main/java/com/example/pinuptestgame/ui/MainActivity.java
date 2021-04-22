0b977918ad96006899052a5c2e1b1c9678a7d38c
VozNick <vmm408@gmail.com>
2021-04-22 21:31:35 +0300

    edit mainActivity and related files

--- app/src/main/java/com/example/pinuptestgame/ui/MainActivity.java
+++ app/src/main/java/com/example/pinuptestgame/ui/MainActivity.java
@@ -1,10 +1,8 @@
-package com.example.pinuptestgame;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
+package com.example.pinuptestgame.ui;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pinuptestgame.databinding.ActivityMainBinding;
 
@@ -15,11 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-//        NavHostFragment navHostFragment =
-//                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-//        if (navHostFragment != null) {
-//            NavController navController = navHostFragment.getNavController();
-//        }
     }
 }
